dc505e1f52ecfa1e7938fe7135978f623faa86b2
EricOT98 <EricOToole@Outlook.com>
2019-01-22 14:00:09 +0000

    Hover image options added

--- src/Button.js
+++ src/Button.js
@@ -28,4 +28,24 @@ class Button {
             this._element.style.visibility = "visible";
         }.bind(this);
     }
+
+    addHoverImage(src) {
+        this._hoverImage = new Image();
+        this._hoverImage.style.display = "block";
+        this._hoverImage.src = src;
+        this._hoverImage.style.display = "none";
+        this._hoverImage.onload = function() {
+            this._element.appendChild(this._hoverImage);
+            this._element.style.padding = "0";
+            this._element.style.visibility = "visible";
+        }.bind(this);
+        this._element.onmouseover = () => {
+            this._img.style.display = "none";
+            this._hoverImage.style.display = "block";
+        };
+        this._element.onmouseleave = () => {
+            this._img.style.display = "block";
+            this._hoverImage.style.display = "none";
+        }
+    }
 }
